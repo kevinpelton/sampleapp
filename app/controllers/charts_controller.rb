2319c11430ba3fbe243ba778f4e27945227979cd
Kevin Pelton <kjpelton@yahoo.com>
2012-05-02 20:30:41 -0400

    fixed charts

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,6 +1,6 @@
 class ChartsController < ApplicationController
 	def index
-#	    @hospitals = Hospital.all
+		@hospitals = Hospital.select("state, hospital_type, count(hospital_type) as total_hospitals").group("hospital_type","state").order("state")
 
     	respond_to do |format|
     	  format.html # index.html.erb
